4d2389c59ad5a326dbb07685a338fc80bcb84b1f
Craig Vitter <craig.vitter@gmail.com>
2018-06-30 22:59:22 -0400

    Add first set of events

--- events.py
+++ events.py
@@ -2,7 +2,13 @@
 Dictionary of supported JIRA events and output friendly format
 """
 jira_events = {
-    "pr:comment:added": "Pull Request: Comment Added",
-    "pr:comment:deleted": "Pull Request: Comment Deleted",
-    "pr:comment:edited": "Pull Request: Comment Edited"
+    "project_created": "New Project Created",
+    "jira:issue_created": "New Issue Created",
+    "jira:issue_updated": "Issue Updated"
+}
+
+issue_events = {
+    "issue_commented": "Comment Added",
+    "issue_comment_edited": "Comment Edited",
+    "issue_comment_deleted": "Comment Deleted"
 }
