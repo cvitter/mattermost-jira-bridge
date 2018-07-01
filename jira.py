5e907292653983ddbfc42f82b682b7181c2aed21
Craig Vitter <craig.vitter@gmail.com>
2018-07-01 12:15:13 -0400

    Just saving updates

--- jira.py
+++ jira.py
@@ -6,7 +6,7 @@ import events
 import projects
 
 
-def readConfig():
+def read_config():
     """
     Reads config.json to get configuration settings
     """
@@ -16,6 +16,9 @@ def readConfig():
     application_host = d["application"]["host"]
     application_port = d["application"]["port"]
     application_debug = d["application"]["debug"]
+
+    global use_attachments
+    use_attachments = d["features"]["use_attachments"]
 
     global error_color, alert_color, success_color
     error_color = d["colors"]["error"]
@@ -31,23 +34,49 @@ def readConfig():
     jira_url = d["jira"]["url"]
 
 
+def get_channel(project_id):
+    return projects.get(project_id, default="")
+
+
+def send_webhook(project_id, text_out, attachment_text, attachment_color):
+    """
+    """
+    if len(project_id) > 0:
+        channel = get_channel(project_id)
+
+    data = {
+        "channel": channel,
+        "text": text_out,
+        "username": mattermost_user,
+        "icon_url": mattermost_icon,
+        "attachments": [attach_dict]
+    }
+
+    response = requests.post(
+        webhook_url,
+        data=json.dumps(data),
+        headers={'Content-Type': 'application/json'}
+    )
+    return response
+
+
 """
 ------------------------------------------------------------------------------------------
 Flask application below
 """
-readConfig()
+read_config()
 
 app = Flask(__name__)
 
 
-@app.route( '/jira/<project_id>', methods = [ 'POST' ] )
+@app.route('/jira/<project_id>', methods=['POST'])
 def hooks(project_id):
 
     if len(request.get_json()) > 0:
         print(project_id)
-        print(request.get_json())
+        print(json.dumps(request.get_json()))
     return ""
 
 if __name__ == '__main__':
-   app.run(host = application_host, port = application_port, 
-           debug = application_debug)
+    app.run(host=application_host, port=application_port,
+            debug=application_debug)
